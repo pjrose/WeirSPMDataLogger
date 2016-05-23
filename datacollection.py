12c18c4dc3a517311e8179318ccfca02c9eb0c28
pjrose <pjrose@gmail.com>
2016-05-23 11:56:54 -0500

    Added in SOT23 and core temp to logged info.
    Also, fan speed will be set by core temp if the TO-92 sensor becomes
    inoperable or is not configured, in which case it reads 0.

--- datacollection.py
+++ datacollection.py
@@ -288,11 +288,7 @@ def get_Database_File_Path():
 
 
 
-def getCPUTemperature():
-    res = os.popen('vcgencmd measure_temp').readline()
-    temp_c = float(res.replace("temp=","").replace("'C\n",""))
-    temp_f = 9.0/5.0*temp_c+32.0
-    return "{0:.2f}".format(temp_c) + 'C, ' + "{0:.2f}".format(temp_f) + ' F'
+
 
 def init_db():
     logging.info('Initializing database...')
--- picofan.py
+++ picofan.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
 import pigpio # http://abyz.co.uk/rpi/pigpio/python.html
+import os
+
+
+
 
 class controller:
    """
@@ -16,12 +20,21 @@ class controller:
       #0x69 13 (0x0D) : temperature in C of TO-92 (fan kit) in BCD (i.e. 0x36 is 36C)
       #see setup notes (or pi modules forum) to get TO-92 sensor working, it's not enabled by default!
 
+       
+
    def __init__(self, pi, i2c_handle_69, i2c_handle_6b):
       self.i2c_handle_69 = i2c_handle_69
       self.i2c_handle_6b = i2c_handle_6b
       self.pi = pi
       self.fan_speed = 0
+      self.fan_speed_str = 'off'
       self.pi.i2c_write_byte_data(self.i2c_handle_6b, 15, 1)
+
+   def getCPUTemperature(self):
+      temp_c = float(os.popen('vcgencmd measure_temp').readline().replace("temp=","").replace("'C\n",""))
+      #temp_f = 9.0/5.0*float+32.0
+      return temp_c
+
        
    def adjust_fan_speed(self):
     
@@ -40,8 +53,16 @@ class controller:
 
       #degrees c of temperature drop required before change fan speed is reduced
       HYSTERESIS = 5
-   
-      temperature = int('{:02x}'.format(self.pi.i2c_read_byte_data(self.i2c_handle_69, 13)))
+
+      ups_TO92_temperature = int('{:02x}'.format(self.pi.i2c_read_byte_data(self.i2c_handle_69, 13)))
+      ups_SOT23_temperature = int('{:02x}'.format(self.pi.i2c_read_byte_data(self.i2c_handle_69, 12)))
+      core_temperature = self.getCPUTemperature()
+
+      if(ups_TO92_temperature == 0):
+         temperature = core_temperature
+      else:
+         temperature = ups_TO92_temperature
+                  
       new_speed = self.fan_speed
       
       if(self.fan_speed is STOP and temperature >= THRESHOLD_25): 
@@ -74,13 +95,27 @@ class controller:
             new_speed =  SPEED_50
          elif(temperature <= (THRESHOLD_25 - HYSTERESIS)):
             new_speed = STOP
-            
 
+      #trap incase TO-92 sensor is not working or not setup properly, default fan to 50%
+      if(temperature == 0):
+         new_speed = SPEED_50
+            
       if(self.fan_speed is not new_speed):
          self.fan_speed = new_speed
          self.pi.i2c_write_byte_data(self.i2c_handle_6b, 16, self.fan_speed)
+
+      if(self.fan_speed == STOP):
+         self.fan_speed_str = 'off'
+      elif(self.fan_speed ==  SPEED_25):
+         self.fan_speed_str = '25%'
+      elif(self.fan_speed ==  SPEED_50):
+         self.fan_speed_str = '50%'
+      elif(self.fan_speed ==  SPEED_75):
+         self.fan_speed_str = '75%'
+      elif(self.fan_speed ==  FULLSPEED):
+         self.fan_speed_str = '100%'     
          
-      return 'Temperature ' + str(temperature) + 'C, fan speed set to ' + str(self.fan_speed)
+      return 'Sys temps: Core ' + str(int(core_temperature)) + 'C,UPS SOT23 ' + str(ups_SOT23_temperature) + 'C, UPS TO-92 '+ str(ups_TO92_temperature) + 'C, fan speed: ' + str(self.fan_speed_str)
 
 
     
